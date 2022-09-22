07f09617760b5b6e408043a2847724041f87e7f8
Artem <artkruzhkov@gmail.com>
2022-09-22 23:45:56 +0300

    task1anothersolution

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter 5 digits number ");
+int N = int.Parse(Console.ReadLine()!);
+int num1 = N / 10000;
+int num2 = N % 10;
+int num3 = N / 1000;
+int num4 = num3 % 10;
+int num5 = N / 10;
+int num6 = num5 % 10;
+if((num1 == num2)&&(num4 == num6))
+{
+    Console.WriteLine("palidrom");
+}
+else
+{
+    Console.WriteLine("Not palidrom");
+}
